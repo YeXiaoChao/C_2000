22f36705c82677016b864962bd373ded4975597f
YeXiaoChao <yechao@ycdoit.com>
2015-04-08 10:33:28 +0800

    create

---
+++ 2_2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _2_2
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Get = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Get
+            // 
+            this.Btn_Get.Location = new System.Drawing.Point(106, 28);
+            this.Btn_Get.Name = "Btn_Get";
+            this.Btn_Get.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Get.TabIndex = 0;
+            this.Btn_Get.Text = "输出字符串";
+            this.Btn_Get.UseVisualStyleBackColor = true;
+            this.Btn_Get.Click += new System.EventHandler(this.btn_Get_Click);
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 75);
+            this.Controls.Add(this.Btn_Get);
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "使用引号运算符进行赋值";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Get;
+    }
+}
+
---
+++ 2_3_Calculator/Frm_Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2_3_Calculator
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_num_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
